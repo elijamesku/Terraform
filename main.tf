4a0d502396e09fe70964bbbe6e96e9605654ed6f
Eli James <ejames@lead.bank>
2025-08-29 13:53:46 -0500

    new change

--- main.tf
+++ main.tf
@@ -2,29 +2,42 @@ provider "aws" {
   region = "us-east-2"
 }
 
-resource "aws_instance" "example" {
-  ami                         = "ami-0fb653ca2d3203ac1"
-  instance_type               = "t2.micro"
-  vpc_security_group_ids      = [aws_security_group.instance]
-  user_data                   = <<-EOF
-              #!/bin/bash
-              echo "Hello welcome to the web server" > index.html
-              nohup busybox httpd -f -p 8080 &
-              EOF
-  user_data_replace_on_change = true
 
-  tags = {
-    Name = "Terraform-EC2-1"
-  }
-}
-
-resource "aws_security_group" "instance" {
-  name = "Terraform-EC2-1-instance"
+resource "aws_security_group" "web" {
+  name        = "Terraform-EC2-1-instance"
+  description = "Allow HTTP from anywhere"
   ingress {
-    from_port   = 8080
-    to_port     = 8080
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  #allowing outbound traffic
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "example" {
+  ami                         = "ami-0fb653ca2d3203ac1"
+  instance_type               = "t2.micro"
+  vpc_security_group_ids      = [aws_security_group.web.id]
+  associate_public_ip_address = true
+  user_data_replace_on_change = true
+
+  user_data = <<-EOF
+              #!/bin/bash
+              set -euxo pipefail
+              mkdir -p /opt/web
+              echo "Hello welcome to the web server" > /opt/web/index.html
+              cd /opt/web
+              nohup python3 -m http.server 80 > /var/log/web.log 2>&1 &
+              EOF
+  tags = {
+    Name = "Terraform-EC2-1"
+  }
 }

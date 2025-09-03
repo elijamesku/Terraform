2d5d2bad372f31d6106f557e0b60bcc7b8947e60
Eli James <ejames@lead.bank>
2025-09-02 22:53:17 -0500

    added listener to load balancer

--- main.tf
+++ main.tf
@@ -106,3 +106,19 @@ resource "aws_lb" "loadbalancer" {
   subnets            = data.aws_subnets.default.ids
 
 }
+
+resource "aws_lb_listener" "http" {
+  load_balancer_arn = aws_lb.loadbalancer.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type = "fixed-response"
+
+    fixed_response {
+      content_type = "text/plain"
+      message_body = "404: page not found"
+      status_code  = 404
+    }
+  }
+}

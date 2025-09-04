99d6a9c8cf0a6fc7ee54cf8045cb2edbc8812887
Eli James <ejames@lead.bank>
2025-09-04 16:39:59 -0500

    Changed s3 key

--- aws/dev/backend.tf
+++ aws/dev/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket       = "ss33-bucket"
-    key          = "state/terraform.tfstate"
+    key          = "aws/dev/terraform.tfstate"
     region       = "us-east-2"
     encrypt      = true
     use_lockfile = true

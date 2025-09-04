f22789282b4f8ed34ecc551430333b840277d97e
Eli James <ejames@lead.bank>
2025-09-04 12:49:33 -0500

    comments added

--- main.tf
+++ main.tf
@@ -22,6 +22,7 @@ resource "aws_s3_bucket_versioning" "enabled" {
 
 }
 
+#adding encryption to s3
 resource "aws_s3_bucket_server_side_encryption_configuration" "default" {
   bucket = aws_s3_bucket.terraform-state.id
 

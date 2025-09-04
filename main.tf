1b02f61d9e0a5a7942a123d220b26184c57b02bd
Eli James <ejames@lead.bank>
2025-09-03 19:21:56 -0500

    egress rule for ALB

--- main.tf
+++ main.tf
@@ -141,5 +141,12 @@ resource "aws_security_group" "alb" {
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+  egress = {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+
+  }
 
 }

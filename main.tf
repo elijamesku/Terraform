e0200576ecfb2140e2a9ac99516692e3d0db0622
Eli James <ejames@lead.bank>
2025-09-03 19:16:56 -0500

    ingress rules for ALB security group

--- main.tf
+++ main.tf
@@ -131,3 +131,15 @@ resource "aws_lb_listener" "http" {
     }
   }
 }
+
+resource "aws_security_group" "alb" {
+  name = "terraform-example-alb"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+}
